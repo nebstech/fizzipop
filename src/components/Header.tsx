59d7957b575be166039ccddbd030c7159471ce62
Adonis <adonis.casado@outlook.com>
2024-09-17 17:16:09 -0400

    all

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FizziLogo } from '@/components/FizziLogo'
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <header className='flex justify-center py-4 -mb-28'>
+      <FizziLogo className='h-20 z-10 cursor-pointer text-sky-800' />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,61 @@
+import { Content } from "@prismicio/client";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { asText } from "@prismicio/client";
+
+import { Bounded } from "@/components/Boumded";
+import Button from "@/components/Button";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div className="grid">
+        <div className="grid h-screen place-items-center">
+          <div className="grid auto-rows-min place-items-center text-center">
+            <h1 className="hero-header text-7xl font-black uppercase leading-[.8] text-orange-500 md:text-[9rem] lg:text-[13rem]">
+              {asText(slice.primary.heading)}
+            </h1>
+            <div className="hero-subheading mt-12 text-5xl font-semibold text-sky-950 lg:text-6xl">
+              <PrismicRichText field={slice.primary.subheading} />
+            </div>
+            <div className="hero-body text-2xl font-normal text-sky-950">
+              <PrismicRichText field={slice.primary.body} />
+            </div>
+            <Button
+              buttonLink={slice.primary.button_link}
+              buttonText={slice.primary.button_text}
+              className="hero-button mt-12"
+            />
+          </div>
+        </div>
+
+        <div className="text-side relative z-[80] grid h-screen items-center gap-4 md:grid-cols-2">
+          <PrismicNextImage
+            className="w-full md:hidden"
+            field={slice.primary.cans_image}
+          />
+          <div>
+
+          <h2 className="text-side-heading text-balance text-6xl font-black uppercase text-sky-950 lg:text-8xl">
+            <PrismicRichText field={slice.primary.second_heading} />
+          </h2>
+          <PrismicRichText field={slice.primary.second_body} />
+          </div>
+        </div>
+      </div>
+    </Bounded>
+  );
+};
+
+export default Hero;
