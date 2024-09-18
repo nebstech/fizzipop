9c3764fb69c112b77d273f599898427e0483e68a
Adonis <adonis.casado@outlook.com>
2024-09-18 16:53:14 -0400

    header

---
+++ src/components/ViewCanvas.tsx
@@ -0,0 +1,15 @@
+'use-client';
+
+import { Canvas } from "@react-three/fiber"
+
+
+
+type Props = {}
+
+const ViewCanvas = (props: Props) => {
+  return (
+    <Canvas>ViewCanvas</Canvas>
+  )
+}
+
+export default ViewCanvas
